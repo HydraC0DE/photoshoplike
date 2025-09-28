998ff156b74c85cfc63f28918e02e39361ff2ac0
HydraC0DE <szaboakos0409@gmail.com>
2025-09-28 22:21:26 +0200

    Grayscale faster version added

--- imageprocessor/imageprocessor/Filters/Grayscale.cs
+++ imageprocessor/imageprocessor/Filters/Grayscale.cs
@@ -11,73 +11,8 @@ namespace imageprocessor.Filters
 {
     internal class Grayscale
     {
-        //public Bitmap Apply8BitImage(Bitmap bitmapOriginal) //new image with 8bitdepth grayscale 
-        //{
-        //    // clone the original bitmap so parallel processing is safe later
-        //    Bitmap safeBitmap = (Bitmap)bitmapOriginal.Clone();
-
-        //    int width = safeBitmap.Width;  
-        //    int height = safeBitmap.Height;
-
-        //    // reduce to 8 bit greyscale image and index pixels
-        //    Bitmap grayBitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
-        //    ColorPalette palette = grayBitmap.Palette;
-        //    for (int i = 0; i < 256; i++)
-        //    {
-        //        palette.Entries[i] = Color.FromArgb(i, i, i); //fill up palette from black to white
-        //    }
-        //    grayBitmap.Palette = palette;
-
-        //    BitmapData sourceData = safeBitmap.LockBits( //lock the source
-        //        new Rectangle(0, 0, width, height),//entire image
-        //        ImageLockMode.ReadOnly,
-        //        PixelFormat.Format24bppRgb);
-
-        //    BitmapData grayData = grayBitmap.LockBits( //lock the destination
-        //        new Rectangle(0, 0, width, height),
-        //        ImageLockMode.WriteOnly,
-        //        PixelFormat.Format8bppIndexed);
-
-        //    int srcStride = sourceData.Stride; //byte amount in memory, per row, including padding so we dont index too early 
-        //    int grayStride = grayData.Stride;
-
-        //    byte[] srcBuffer = new byte[srcStride * height]; //big enough arrays for pixel data
-        //    byte[] grayBuffer = new byte[grayStride * height];
-
-        //    Marshal.Copy(sourceData.Scan0, srcBuffer, 0, srcBuffer.Length);//pointer to first byte, number of bytes in a row, 0 startindex, length
-
-        //    Parallel.For(0, height, y => //parallel for to optimize runtime, each thread works on different rows so safew
-        //    {
-        //        int srcOffset = y * srcStride; //offset by padding bytes in memory
-        //        int grayOffset = y * grayStride;
-        //        for (int x = 0; x < width; x++)
-        //        { // b g r order for no reason at all ?
-        //            byte b = srcBuffer[srcOffset + x * 3]; //get byte values from source
-        //            byte g = srcBuffer[srcOffset + x * 3 + 1];
-        //            byte r = srcBuffer[srcOffset + x * 3 + 2];
-
-        //            //luminosity formula
-        //            byte gray = (byte)(0.299 * r + 0.587 * g + 0.114 * b);
-
-        //            grayBuffer[grayOffset + x] = gray;
-        //        }
-        //    });
-
-        //    Marshal.Copy(grayBuffer, 0, grayData.Scan0, grayBuffer.Length);//copy to destination address
-
-        //    safeBitmap.UnlockBits(sourceData);
-        //    grayBitmap.UnlockBits(grayData);
-
-        //    safeBitmap.Dispose(); // free cloned bitmap memory
-
-        //    return grayBitmap;
-        //}
-
         public Bitmap Apply8BitImage(Bitmap bitmapOriginal) //new image with 8bitdepth grayscale 
         {
-            // clone the original bitmap so parallel processing is safe later
-            //Bitmap safeBitmap = (Bitmap)bitmapOriginal.Clone();
-
             int width = bitmapOriginal.Width;
             int height = bitmapOriginal.Height;
 
@@ -103,7 +38,6 @@ namespace imageprocessor.Filters
             int srcStride = sourceData.Stride; //byte amount in memory, per row, including padding so we dont index too early 
             int grayStride = grayData.Stride;
 
-            //Marshal.Copy(sourceData.Scan0, srcBuffer, 0, srcBuffer.Length);//pointer to first byte, number of bytes in a row, 0 startindex, length
             unsafe
             {
                 byte* srcPtr = (byte*)sourceData.Scan0; // pointer to first source pixel
@@ -136,29 +70,45 @@ namespace imageprocessor.Filters
             return grayBitmap;
         }
 
-        public Bitmap ApplyFaster(Bitmap bitmapOriginal)
+        public Bitmap FastApply(Bitmap bitmapOriginal)
         {
-            BitmapData bmData = bitmapOriginal.LockBits(new Rectangle(0, 0, bitmapOriginal.Width, bitmapOriginal.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
-            int stride = bmData.Stride;
-            System.IntPtr Scan0 = bmData.Scan0;
+            int width = bitmapOriginal.Width;
+            int height = bitmapOriginal.Height;
+
+            BitmapData bitmapData = bitmapOriginal.LockBits(
+                new Rectangle(0, 0, bitmapOriginal.Width, bitmapOriginal.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format24bppRgb);
+
+            int stride = bitmapData.Stride; //byte amount in memory, per row, including padding so we dont index too early 
+            IntPtr Scan0 = bitmapData.Scan0; //first byte pointer
+
             unsafe
             {
-                byte* p = (byte*)(void*)Scan0;
-                int nOffset = stride - bitmapOriginal.Width * 3;
-                int nWidth = bitmapOriginal.Width * 3;
-                for (int y = 0; y < bitmapOriginal.Height; ++y)
+                byte* basePointer = (byte*)Scan0;
+
+                Parallel.For(0, height, y =>
                 {
-                    for (int x = 0; x < nWidth; ++x)
+                    byte* row = basePointer + (y * stride); // pointer to start of row
+
+                    for (int x = 0; x < width; ++x)
                     {
-                        p[0] = (byte)(255 - p[0]);
-                        ++p;
+                        byte b = row[0];
+                        byte g = row[1];
+                        byte r = row[2];
+
+                        // grayscale formula
+                        byte gray = (byte)(.299 * r + .587 * g + .114 * b);
+
+                        row[0] = row[1] = row[2] = gray;
+
+                        row += 3; // move to next pixel
                     }
-                    p += nOffset;
-                }
+                });
             }
 
-            bitmapOriginal.UnlockBits(bmData);
-            return null;
+            bitmapOriginal.UnlockBits(bitmapData);
+            return bitmapOriginal;
         }
     }
 }
