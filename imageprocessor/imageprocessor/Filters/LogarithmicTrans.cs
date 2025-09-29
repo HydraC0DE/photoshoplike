93261c3c584c081eef8ba28df61b5dbae2fa7f73
HydraC0DE <szaboakos0409@gmail.com>
2025-09-29 15:29:45 +0200

    logarithmic and small tests

--- imageprocessor/imageprocessor/Filters/LogarithmicTrans.cs
+++ imageprocessor/imageprocessor/Filters/LogarithmicTrans.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace imageprocessor.Filters
 {
-    internal class LogarithmicTrans
+    public class LogarithmicTrans
     {
+        public Bitmap ApplyLogartihmic(Bitmap bitmapOriginal, double normalizationFactor = -1)
+        {
+            if (normalizationFactor == -1 || normalizationFactor<0) //user didn't specify value so lets go with 46 which is apperently good for this
+            {
+                normalizationFactor = normalizationFactor = 255.0 / Math.Log(1 + 255);
+            }
+            //should be between 1 and 100, will specify this in UI
+
+            int width = bitmapOriginal.Width;
+            int height = bitmapOriginal.Height;
+
+            BitmapData bitmapData = bitmapOriginal.LockBits(
+        new Rectangle(0, 0, width, height),
+        ImageLockMode.ReadWrite,
+        PixelFormat.Format24bppRgb);
+
+            int stride = bitmapData.Stride;
+            IntPtr Scan0 = bitmapData.Scan0;
+
+            byte[] logTable = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                logTable[i] = (byte)Math.Min(255, normalizationFactor * Math.Log(1 + i));
+            }
+
+          
+
+            unsafe
+            {
+                byte* basePointer = (byte*)Scan0;
+                Parallel.For(0, height, y =>
+                {
+                    byte* row = basePointer + (y * stride); // pointer to start of row
+
+                    for (int x = 0; x < width; x++)
+                    {
+                        row[0] = logTable[row[0]];
+                        row[1] = logTable[row[1]];
+                        row[2] = logTable[row[2]];
+                        
+
+                        row += 3; // move to next pixel
+                    }
+                });
+            }
+
+            bitmapOriginal.UnlockBits(bitmapData);
+            return bitmapOriginal;
+        }
     }
 }
--- imageprocessor/imageprocessor/Program.cs
+++ imageprocessor/imageprocessor/Program.cs
@@ -15,36 +15,53 @@ namespace imageprocessor
 
             string outputPath = @"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\output_uglycat_gray.bmp";
             grayImage.Save(outputPath);
-
+            //negate
             rawUglycat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\uglycat_raw.png");
             Negation negation = new Negation();
             Bitmap grayImageHighBitDepth = negation.Negate(rawUglycat);
             string outputPath2 = @"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\output_uglycat_negate.bmp";
             grayImageHighBitDepth.Save(outputPath2);
 
-            rawUglycat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\uglycat_raw.png");
 
-            // --- Darker gamma ---
+
+            // darker gamma
+            rawUglycat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\uglycat_raw.png");
             GammaTrans gammaDarker = new GammaTrans();
-            Bitmap darker = gammaDarker.ApplyGamma(new Bitmap(rawUglycat), 0.5, 0.5, 0.5); // higher gamma = darker
+            Bitmap darker = gammaDarker.ApplyGamma(new Bitmap(rawUglycat), 0.5, 0.5, 0.5); // lower gamma = darker
             string outputPathDarker = @"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\output_uglycat_gamma_darker.bmp";
             darker.Save(outputPathDarker);
 
-            rawUglycat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\uglycat_raw.png");
 
-            // --- Lighter gamma ---
+
+            // ligher gamma
+            rawUglycat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\uglycat_raw.png");
             GammaTrans gammaLighter = new GammaTrans();
-            Bitmap lighter = gammaLighter.ApplyGamma(new Bitmap(rawUglycat), 3, 3, 3); // lower gamma = lighter
+            Bitmap lighter = gammaLighter.ApplyGamma(new Bitmap(rawUglycat), 3, 3, 3); // higher gamma = lighter
             string outputPathLighter = @"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\output_uglycat_gamma_lighter.bmp";
             lighter.Save(outputPathLighter);
 
-            rawUglycat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\uglycat_raw.png");
 
-            // --- Only blue channel adjusted ---
+
+            // blue gamma
+            rawUglycat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\uglycat_raw.png");
             GammaTrans gammaBlue = new GammaTrans();
             Bitmap blueBoost = gammaBlue.ApplyGamma(new Bitmap(rawUglycat), 1, 1, 2); // make blue darker
             string outputPathBlue = @"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\output_uglycat_gamma_blue.bmp";
             blueBoost.Save(outputPathBlue);
+
+            //logarithmicHighValue
+            Bitmap rawDarkScaryCat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\darkscarycat_raw.png");
+            LogarithmicTrans logarithmicTooHigh = new LogarithmicTrans();
+            Bitmap logtranshigh = logarithmicTooHigh.ApplyLogartihmic(new Bitmap(rawDarkScaryCat), 70);
+            string outputPathLogaritmicHigh = @"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\output_scarycat_log_high.bmp";
+            logtranshigh.Save(outputPathLogaritmicHigh);
+
+            //logarithmicBaseValue
+            rawDarkScaryCat = new Bitmap(@"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\darkscarycat_raw.png");
+            LogarithmicTrans logarithmicBaseVal = new LogarithmicTrans();
+            Bitmap logtransbase = logarithmicBaseVal.ApplyLogartihmic(new Bitmap(rawDarkScaryCat));
+            string outputPathLogaritmicBase = @"D:\DEVELOPMENT\DevRepos\photoshoplike\pictures\output_scarycat_log_base.bmp";
+            logtransbase.Save(outputPathLogaritmicBase);
             ;
         }
     }
