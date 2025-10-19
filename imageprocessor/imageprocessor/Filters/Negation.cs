cc726f7e5a4b4800cc3f3934581eaf19b39ff2bb
HydraC0DE <szaboakos0409@gmail.com>
2025-10-19 19:36:36 +0200

    Negation optimized

--- imageprocessor/imageprocessor/Filters/Negation.cs
+++ imageprocessor/imageprocessor/Filters/Negation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -10,7 +11,7 @@ namespace imageprocessor.Filters
 {
     public class Negation
     {
-        public Bitmap Negate(Bitmap bitmapOriginal)
+        public Bitmap NegateOld(Bitmap bitmapOriginal)//100 ms
         {
             int width = bitmapOriginal.Width;
             int height = bitmapOriginal.Height;
@@ -39,6 +40,78 @@ namespace imageprocessor.Filters
             }
 
             bitmapOriginal.UnlockBits(bitmapData);
+            return bitmapOriginal;
+        }
+
+        public Bitmap Negate(Bitmap bitmapOriginal) //73 ms
+        {
+            int width = bitmapOriginal.Width;
+            int height = bitmapOriginal.Height;
+
+            BitmapData bmpData = bitmapOriginal.LockBits(
+                new Rectangle(0, 0, width, height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format24bppRgb);
+
+            int stride = bmpData.Stride;
+            IntPtr scan0 = bmpData.Scan0;
+
+            unsafe
+            {
+                byte* basePtr = (byte*)scan0;
+
+                // Partition rows into chunks for parallel threads
+                var range = Partitioner.Create(0, height, Math.Max(1, height / Environment.ProcessorCount));
+
+                Parallel.ForEach(range, (rangeSegment) =>
+                {
+                    for (int y = rangeSegment.Item1; y < rangeSegment.Item2; y++)
+                    {
+                        byte* row = basePtr + y * stride;
+                        int x = 0;
+
+                        // Process 4 pixels per iteration (loop unrolling)
+                        int maxUnroll = width - 3;
+                        for (; x < maxUnroll; x += 4)
+                        {
+                            // pixel 0
+                            row[0] = (byte)(255 - row[0]);
+                            row[1] = (byte)(255 - row[1]);
+                            row[2] = (byte)(255 - row[2]);
+                            row += 3;
+
+                            // pixel 1
+                            row[0] = (byte)(255 - row[0]);
+                            row[1] = (byte)(255 - row[1]);
+                            row[2] = (byte)(255 - row[2]);
+                            row += 3;
+
+                            // pixel 2
+                            row[0] = (byte)(255 - row[0]);
+                            row[1] = (byte)(255 - row[1]);
+                            row[2] = (byte)(255 - row[2]);
+                            row += 3;
+
+                            // pixel 3
+                            row[0] = (byte)(255 - row[0]);
+                            row[1] = (byte)(255 - row[1]);
+                            row[2] = (byte)(255 - row[2]);
+                            row += 3;
+                        }
+
+                        // Remaining pixels
+                        for (; x < width; x++)
+                        {
+                            row[0] = (byte)(255 - row[0]);
+                            row[1] = (byte)(255 - row[1]);
+                            row[2] = (byte)(255 - row[2]);
+                            row += 3;
+                        }
+                    }
+                });
+            }
+
+            bitmapOriginal.UnlockBits(bmpData);
             return bitmapOriginal;
         }
     }
